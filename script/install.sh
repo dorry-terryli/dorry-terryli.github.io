889537e270a53abb81767f6363d08e0eafcbfee2
terry.li <terry.li@dorry.io>
2017-04-08 15:53:48 +0800

    foo

--- script/install.sh
+++ script/install.sh
@@ -6,7 +6,7 @@ sudo pip3 install pymongo
 wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-script.tar.gz
 mkdir ~/dorry-alpha/
 mkdir ~/dorry-alpha/dorry-installation
-tar -xvzf release-script.tar.gz -C ~/dorry/dorry-installation
+tar -xvzf release-script.tar.gz -C ~/dorry-alpha/dorry-installation
 python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_kubernetes.py
 python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_platform.py
 sudo rm -r ~/dorry-alpha
