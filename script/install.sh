989fe1bc58ffeb02fb4f67379a0ff3a7578be4de
terry.li <terry.li@dorry.io>
2017-04-08 15:25:12 +0800

    change kubeapi

--- script/install.sh
+++ script/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-apt install python3
-apt install python3-pip
-pip3 install pymongo
+sudo apt install python3
+sudo apt install python3-pip
+sudo pip3 install pymongo
 wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-script.tar.gz
-mkdir /opt/dorry-installation
+sudo mkdir /opt/dorry-installation
 tar -xvzf release-script.tar.gz -C /opt/dorry-installation
 python3 /opt/dorry-installation/MatchboxDorry-dorry-installation-*/install_kubernetes.py
 python3 /opt/dorry-installation/MatchboxDorry-dorry-installation-*/install_platform.py
-rm -r /opt/dorry-installation
+sudo rm -r /opt/dorry-installation
