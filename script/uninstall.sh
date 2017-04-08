1e8e65148c09ddb4d9a9a0d4be5e690ba8e68413
terry.li <terry.li@dorry.io>
2017-04-08 16:00:09 +0800

    foo

--- script/uninstall.sh
+++ script/uninstall.sh
@@ -4,5 +4,5 @@ wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-
 mkdir ~/dorry-alpha
 mkdir ~/dorry-alpha/dorry-installation
 tar -xvzf release-script.tar.gz -C ~/dorry-alpha/dorry-installation
-python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_kubernetes.py
-python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_platform.py
+sudo python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_kubernetes.py
+sudo python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_platform.py
