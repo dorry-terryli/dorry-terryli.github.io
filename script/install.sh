fca64e2236c4a38d4702fd2e23217ccd943523d1
terry.li <terry.li@dorry.io>
2017-04-08 15:48:35 +0800

    foo

--- script/install.sh
+++ script/install.sh
@@ -4,9 +4,9 @@ sudo apt install python3
 sudo apt install python3-pip
 sudo pip3 install pymongo
 wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-script.tar.gz
-mkdir ~/dorry/
-mkdir ~/dorry/dorry-installation
+mkdir ~/dorry-alpha/
+mkdir ~/dorry-alpha/dorry-installation
 tar -xvzf release-script.tar.gz -C ~/dorry/dorry-installation
-python3 ~/dorry/dorry-installation/MatchboxDorry-dorry-installation-*/install_kubernetes.py
-python3 ~/dorry/dorry-installation/MatchboxDorry-dorry-installation-*/install_platform.py
-sudo rm -r ~/dorry/dorry-installation
+python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_kubernetes.py
+python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_platform.py
+sudo rm -r ~/dorry-alpha
--- script/uninstall.sh
+++ script/uninstall.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-script.tar.gz
-mkdir ~/dorry
-mkdir ~/dorry/dorry-installation
-tar -xvzf release-script.tar.gz -C /opt/dorry-installation
-python3 ~/dorry/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_kubernetes.py
-python3 ~/dorry/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_platform.py
+mkdir ~/dorry-alpha
+mkdir ~/dorry-alpha/dorry-installation
+tar -xvzf release-script.tar.gz -C ~/dorry-alpha/dorry-installation
+python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_kubernetes.py
+python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/uninstall_platform.py
