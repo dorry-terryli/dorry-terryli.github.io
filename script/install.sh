90a656388bd6b2918280bb064cab97fce8ba45f3
terry.li <terry.li@dorry.io>
2017-04-10 15:49:58 +0800

    modi install script

--- script/install.sh
+++ script/install.sh
@@ -6,6 +6,8 @@ sudo pip3 install pymongo
 wget https://bitbucket.org/MatchboxDorry/dorry-installation/get/feature/release-script.tar.gz
 mkdir ~/dorry-alpha/
 mkdir ~/dorry-alpha/dorry-installation
+echo 'export KUBECONFIG=$HOME/admin.conf' >> $HOME/.bashrc
+source $HOME/.bashrc
 tar -xvzf release-script.tar.gz -C ~/dorry-alpha/dorry-installation
 python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_kubernetes.py
 sudo python3 ~/dorry-alpha/dorry-installation/MatchboxDorry-dorry-installation-*/install_platform.py
